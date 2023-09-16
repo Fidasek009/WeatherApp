66ca493c8d1c9bbee1270b9fa818a0b3fa40d899
Fidasek009 <fidasek009@gmail.com>
2023-09-16 19:50:53 +0200

    weather api

--- web.py
+++ web.py
@@ -1,22 +1,38 @@
 from os import path
 
+import requests
 from uvicorn import run
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 
 
-HOST="127.0.0.1"
-PORT = 1443
+HOST="5.182.17.134"
+PORT = 4080
 WEBDIR = "{0}/html".format(path.dirname(__file__))
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory=f"{WEBDIR}/static"), name="static")
 
 
+url = "https://tools.keycdn.com/geo.json?host="
+headers = {
+    "User-Agent": "keycdn-tools:https://.kenolas.xyz"
+}
+
+
 @app.get("/")
 def weather(request: Request):
     client_host = request.client.host
-    return {"client_host": client_host}
+
+    # response = requests.get(url + client_host, headers=headers)
+    # location = response.json()['data']['geo']
+
+    # weather = request.get(f"https://api.weatherapi.com/v1/current.json?key=8da8951ed8694b8782c90333231609&q={location['latitude']},{location['longitude']}")
+    weather = requests.get(f"https://api.weatherapi.com/v1/current.json?key=8da8951ed8694b8782c90333231609&aqi=no&q={client_host}")
+
+    
+
+    return weather.json()
 
 
 
