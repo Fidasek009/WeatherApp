81b6ef50d25d7fd7574becac0a2f4904bf830145
Fidasek009 <fidasek009@gmail.com>
2023-09-17 12:46:25 +0200

    first working website

--- web.py
+++ web.py
@@ -5,11 +5,13 @@ from uvicorn import run
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 
 HOST="5.182.17.134"
 PORT = 4080
 WEBDIR = "{0}/html".format(path.dirname(__file__))
+TEMPLATES = Jinja2Templates(directory=f"{WEBDIR}/templates")
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory=f"{WEBDIR}/static"), name="static")
@@ -28,14 +30,13 @@ def weather(request: Request):
 
     # response = requests.get(url + client_host, headers=headers)
     # location = response.json()['data']['geo']
-
     # weather = request.get(f"https://api.weatherapi.com/v1/current.json?key=8da8951ed8694b8782c90333231609&q={location['latitude']},{location['longitude']}")
+
     weather = requests.get(f"https://api.weatherapi.com/v1/current.json?key=8da8951ed8694b8782c90333231609&aqi=no&q={client_host}")
     condition = weather.json()['current']['condition']
 
-    return HTMLResponse(f"<body><h1><img src=\"https:{condition['icon']}\" />{condition['text']}</h1><p>{weather.json()}</p></body>")
-
-    return weather.json()
+    return TEMPLATES.TemplateResponse("site.j2", {"request": request, "condition": condition})
+    
 
 
 
